04d58486404ffcd3ec3eb9d2731b8d37ae503ad5
student <you@example.com>
2023-02-02 21:42:18 +0300

    studentRecords Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Jane"
+    var age: Byte = 19
+    var phoneNumber: Int = 791752690
+    var weightInKg : Float = 49.50F
+    var citizen : Boolean = true
+    println(fullName)
+
+
+}
