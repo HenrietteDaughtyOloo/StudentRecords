60630fd021c44c62e5185bf3f474d945d5a6ecd8
student <you@example.com>
2023-02-03 09:38:52 +0300

    further changes commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
 fun main(args: Array<String>) {
     var fullName: String = "Jane"
+    fullName = "Janet"
     var age: Byte = 19
+    age = 21
     var phoneNumber: Int = 791752690
+    phoneNumber = 723456709
     var weightInKg : Float = 49.50F
+    weightInKg = 60F
     var citizen : Boolean = true
+    citizen = false
     println(fullName)
     println(age)
     println(phoneNumber)
